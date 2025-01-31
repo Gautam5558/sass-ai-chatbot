0c93abd91b490d6da854d979622f6d6710a1fc1e
Gautam <gautam@WIlASSETA0098.local>
2025-01-31 12:49:11 +0530

    Added basic chat button ui on scrolling 200px from top

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,14 +1,47 @@
+"use client";
 import Features from "@/components/features";
 import Hero from "@/components/hero";
 import Pricing from "@/components/pricing";
-import React from "react";
+import { ChevronsDownUp, MessageCircleMore } from "lucide-react";
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [isInView, setIsInView] = useState(false);
+  const [isChatModalOpen, setIsChatModalOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setIsInView(true);
+      } else {
+        setIsInView(false);
+      }
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleChatModal = () => {
+    setIsChatModalOpen((prev) => {
+      return !prev;
+    });
+  };
+
   return (
     <div>
       <Hero />
       <Features />
       <Pricing />
+      {isInView && (
+        <div
+          onClick={toggleChatModal}
+          className="w-10 h-10 rounded-[50%] bg-black text-white p-1 fixed bottom-4 right-4 flex justify-center items-center"
+        >
+          {isChatModalOpen ? <ChevronsDownUp /> : <MessageCircleMore />}
+        </div>
+      )}
     </div>
   );
 };
